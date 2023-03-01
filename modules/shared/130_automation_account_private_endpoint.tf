df93b23d07d996161eeabf41b1abfc26c5fee91e
gutt02 <sven.guttmann@gmail.com>
2023-03-01 20:10:42 +0100

    refactor

--- modules/shared/130_automation_account_private_endpoint.tf
+++ modules/shared/130_automation_account_private_endpoint.tf
@@ -1,26 +1,3 @@
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/automation_account
-resource "azurerm_automation_account" "this" {
-  name                = "${var.project.customer}-${var.project.name}-${var.project.environment}-aacc"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.mgmt.name
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  public_network_access_enabled = false
-
-  sku_name = "Basic"
-}
-
-# The automation account needs read access to the subscription to log in.
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment
-resource "azurerm_role_assignment" "automation_account" {
-  scope                = data.azurerm_subscription.subscription.id
-  role_definition_name = "Reader"
-  principal_id         = azurerm_automation_account.this.identity[0].principal_id
-}
-
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/private_endpoint
 # https://docs.microsoft.com/en-us/azure/private-link/private-endpoint-overview#private-link-resource
 resource "azurerm_private_endpoint" "automation_account" {
